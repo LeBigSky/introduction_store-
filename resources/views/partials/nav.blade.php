d5e207bd062591f14f10ddcf38ee083cef213eeb
Hackashi <nexagray@gmail.com>
2022-11-02 17:04:01 +0100

    add navbar

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,18 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">EXO CRUDS STORE 1</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('home') }}">ENTER DATA</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('creation') }}">SHOW NAMES</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+</header>
